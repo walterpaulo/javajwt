1162d97c3c145933108e1d4a102138da1876a216
walterpaulo <walter0paulo@hotmail.com>
2022-05-28 10:44:20 -0300

    init

---
+++ src/main/java/com/demo/javajwt/JavajwtApplication.java
@@ -0,0 +1,13 @@
+package com.demo.javajwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavajwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/javajwt/JavajwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.javajwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavajwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
